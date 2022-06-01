21c85a87850b1232b265e08e9e9de1ed388a78e0
anilbothe <anil_bothe@yahoo.com>
2022-06-01 09:14:53 +0530

    super, class & other

--- class_obj.py
+++ class_obj.py
@@ -1,5 +1,6 @@
 # Attributes OR Properties CRUD
 
+"""
 class A:
     name = "Anil"
     pass
@@ -14,3 +15,25 @@ print(obj.name)
 del obj.name
 
 print(obj.name)
+
+"""
+
+class A:
+    name = "A"
+    def __init__(self, ) -> None:
+        print("init 1st class")
+        self.secrete_value = 1234
+
+    def myFunc1(self):
+        print("my func1 exec") 
+
+class B(A):
+    def __init__(self, x) -> None:
+        super().__init__()
+        self.x = x
+
+    def myFunc2(self):
+        print("my func2 exec")
+
+obj = B("anil")
+print(obj.secrete_value)
